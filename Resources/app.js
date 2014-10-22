a0fcfe527865bb8489707a59c52671370226715f
Hiroshi Shimoju <hiroshi.shimoju@gmail.com>
2014-10-22 19:09:33 +0900

    TinyTweetを移植

--- Resources/app.js
+++ Resources/app.js
@@ -1,64 +1,40 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
+// ベース色は黒
 Titanium.UI.setBackgroundColor('#000');
 
-// create tab group
+// TabGroupを作成する
 var tabGroup = Titanium.UI.createTabGroup();
 
-
-//
-// create base UI tab and root window
-//
-var win1 = Titanium.UI.createWindow({
-    title:'Tab 1',
-    backgroundColor:'#fff'
-});
 var tab1 = Titanium.UI.createTab({
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
-});
-
-var label1 = Titanium.UI.createLabel({
-  color:'#999',
-  text:'I am Window 1',
-  font:{fontSize:20,fontFamily:'Helvetica Neue'},
-  textAlign:'center',
-  width:'auto'
-});
-
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({
-    title:'Tab 2',
-    backgroundColor:'#fff'
+  icon: 'dark_home.png',
+  title: 'タイムライン',
+  window: Titanium.UI.createWindow({
+    title: 'タイムライン',
+    backgroundColor: '#fff',
+    url: 'timeline.js'
+  })
 });
 var tab2 = Titanium.UI.createTab({
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
+  icon: 'dark_search.png',
+  title: '検索',
+  window: Titanium.UI.createWindow({
+    title: '検索',
+    backgroundColor: '#fff',
+    url: 'search.js'
+  })
+});
+var tab4 =  Titanium.UI.createTab({
+  icon: 'dark_wrench.png',
+  title: '設定',
+  window: Titanium.UI.createWindow({
+    title: '設定',
+    backgroundColor: '#fff',
+    url: 'config.js',
+    barColor: '#000'
+  })
 });
 
-var label2 = Titanium.UI.createLabel({
-  color:'#999',
-  text:'I am Window 2',
-  font:{fontSize:20,fontFamily:'Helvetica Neue'},
-  textAlign:'center',
-  width:'auto'
-});
-
-win2.add(label2);
-
-
-
-//
-//  add tabs
-//
+// タブを追加しTabGroupを表示する
 tabGroup.addTab(tab1);
 tabGroup.addTab(tab2);
-
-
-// open tab group
+tabGroup.addTab(tab4);
 tabGroup.open();
